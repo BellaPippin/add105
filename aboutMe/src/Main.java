3148cfe56ba2aebba132648743da090f28d8615b
Maria C <mcosenza@students.mchenry.edu>
2025-02-09 14:44:11 -0600

    About Me

---
+++ aboutMe/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // I have no idea what I'm doing! So I'll write some comment lines for now.
+    // I am Maria Cosenza and this is Android I. I am more of an Apple girl but
+    // this will be very useful to code apps cross-platform!
+    // This is my first Android class but I'm in iOS II so far and I like it.
+
+    // I hope programming for Android is easy!
+}
