70bc4dc37b35e5fd104ad0820b79d7c8db663c12
Maria C <mcosenza@students.mchenry.edu>
2025-02-10 16:16:36 -0600

    variables

---
+++ variables/src/main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+        val byteValue: Byte = 127
+        val shortValue: Short = 32767
+        val intValue: Int = 2147483647
+        val longValue: Long = 9223372036854775807L
+        val floatValue: Float = 3.14f
+        val doubleValue: Double = 3.141592653589793
+        val booleanValue: Boolean = true
+        val charValue: Char = 'A'
+
+        println("Byte value: $byteValue")
+        println("Short value: $shortValue")
+        println("Int value: $intValue")
+        println("Long value: $longValue")
+        println("Float value: $floatValue")
+        println("Double value: $doubleValue")
+        println("Boolean value: $booleanValue")
+        println("Char value: $charValue")
+
+
+}
