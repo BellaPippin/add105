67c69d98437c179777abe804cb0f112ad2cc0e89
Maria C <mcosenza@students.mchenry.edu>
2025-02-10 16:24:21 -0600

    Operators

---
+++ operators/src/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val a = 10
+    val b = 5
+    val c = 3.5
+    val d = 2.0
+
+    val sum = a + b
+    val diff = a - b
+    val product = a * b
+    val quotient = c / d
+    val isEqual = a == b
+
+    println("Sum: $sum")
+    println("Difference: $diff")
+    println("Product: $product")
+    println("Quotient: $quotient")
+    println("Is equal: $isEqual")
+}
