ef8f01419357ce05980bb896a603a03cd25bff3b
Maria C <mcosenza@students.mchenry.edu>
2025-02-10 17:13:31 -0600

    Hot Dog Stand

---
+++ hotDogStand/src/hotDogStand.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val supplies = arrayOf("Hot dogs", "Buns", "Ketchup", "Mustard", "Relish")
+
+    println("Hot Dog Stand Supplies:")
+    for (item in supplies) {
+        println(item)
+    }
+}
