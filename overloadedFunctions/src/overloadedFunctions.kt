9fa74baf66d669798caadab02cda00e520937168
Maria C <mcosenza@students.mchenry.edu>
2025-02-17 13:13:16 -0600

    Overloading Functions

---
+++ overloadedFunctions/src/overloadedFunctions.kt
@@ -0,0 +1,45 @@
+
+fun sayHello(name: String, ph: String) {
+    println("""
+        Hello $name, I've saved your phone number as $ph.
+    """.trimIndent())
+}
+
+fun sayHello(name: String, ph: String, bizName: String) {
+    println("""
+        Hello $name, we've saved your business as $bizName.
+        We've saved your phone number as $ph.'
+    """.trimIndent())
+}
+
+fun main(){
+
+    println("Are you a residential or business customer?")
+    println("R or B: ")
+    val userChoice= readln()
+
+    if(userChoice.equals("R")){
+        println("Enter name: ")
+        val customerName= readln()
+
+        println("Enter phone number: ")
+        val phoneNumber= readln()
+
+        sayHello(customerName, phoneNumber)
+    } else if (userChoice.equals("B")){
+        println("Enter business name: ")
+        val bizName= readln()
+
+        println("Enter name: ")
+        val customerName= readln()
+
+        println("Enter phone number: ")
+        val phoneNumber= readln()
+
+        sayHello(customerName, phoneNumber, bizName)
+
+    }
+
+
+
+}
