b8017a920ad4224690db77d615bafebda81badfe
Maria C <mcosenza@students.mchenry.edu>
2025-02-24 10:27:19 -0600

    math format

---
+++ math/src/math.kt
@@ -0,0 +1,38 @@
+import java.text.DecimalFormat
+
+// Tip Calculator
+
+fun calculateTipAndTaxes(subtotal: Double): Double{
+
+    val tax = 0.25
+    val tip = 0.175
+
+    var withStuff = subtotal + (subtotal * tax) + (subtotal * tip)
+    return withStuff
+
+}
+
+
+fun main() {
+    var sum = 0.0
+
+    do {
+        println("Enter item price (enter 0 to finish):")
+        val itemPrice = readln().toDouble()
+
+        if (itemPrice != 0.0) {
+            sum += itemPrice
+        }
+
+    } while (itemPrice != 0.0)
+
+    println("Total sum of item prices: $$sum")
+    println("Adding 2.5% Sales Tax...")
+    println("Adding suggested gratuity of 17.5%...\n")
+
+    val formatPlz = DecimalFormat("#.##") //Formatting
+
+    println("Total is: $${formatPlz.format(calculateTipAndTaxes(sum))}")
+
+
+}
