faae47875a81c9672e453eecc422c1e7ac844c8b
Maria C <mcosenza@students.mchenry.edu>
2025-02-24 11:19:55 -0600

    Full Moon Calc

--- fullMoon/src/fullMoon.kt
+++ fullMoon/src/fullMoon.kt
@@ -3,6 +3,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
+    //Define a format for dates, define today.
     val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
     val today = LocalDate.now()
     val formattedToday = today.format(formatter)
@@ -10,10 +11,12 @@ fun main(args: Array<String>) {
     println("Today is $formattedToday.")
     println("The time between full moons is roughly 29 days.\n")
 
+    // Last full moon
     val lastFullMoon: LocalDate = LocalDate.of(2025, 2, 12)
 
     println("The most recent full moon was on $lastFullMoon")
 
+    // Calculate days to next full moon
     val daysToFullMoon = 29
     var nextFullMoon : LocalDate = lastFullMoon.plusDays(daysToFullMoon.toLong())
 
