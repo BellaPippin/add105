92768447d9f586f8dee7241256a94d6421d118fb
Maria C <mcosenza@students.mchenry.edu>
2025-03-11 19:33:54 -0500

    Debug

--- loopingMenu/src/loopingMenu.kt
+++ loopingMenu/src/loopingMenu.kt
@@ -1,5 +1,6 @@
 fun main() {
-    var choice: Int
+    var choice: Int = 0
+
 
     do {
         println("Welcome to McDonald's!")
@@ -11,15 +12,20 @@ fun main() {
         println("5. Exit")
 
         print("Enter your choice (1-5): ")
-        choice = readLine()!!.toInt()
 
-        when (choice) {
-            1 -> println("You chose: View Menu")
-            2 -> println("You chose: Order Burger")
-            3 -> println("You chose: View Prices")
-            4 -> println("You chose: Talk to Staff")
-            5 -> println("You chose: Exit. Have a great day!")
-            else -> println("Invalid choice. Please choose a number between 1 and 5.")
+        try {
+            choice = readln().toInt()
+
+            when (choice) {
+                1 -> println("You chose: View Menu")
+                2 -> println("You chose: Order Burger")
+                3 -> println("You chose: View Prices")
+                4 -> println("You chose: Talk to Staff")
+                5 -> println("You chose: Exit. Have a great day!")
+                else -> println("\nInvalid choice. Please choose a number between 1 and 5.\n")
+            }
+        } catch (e: NumberFormatException) {
+            println("\nException: That's not a number!\n")
         }
     } while (choice != 5)
 }
